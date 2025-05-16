baf302cf634de751db517e57d3a05dad03bdb775
Alexander Mamontov <alexander2555@yandex.ru>
2025-05-16 16:05:29 +0200

    feat: Tasks rendering

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const createTaskElement = task => {
+  const taskItem = document.createElement('div')
+  taskItem.classList.add('task-item')
+  taskItem.dataset.taskId = task.id
+  taskItem.innerHTML = `
+      <div class="task-item__main-container">
+          <div class="task-item__main-content">
+              <form class="checkbox-form">
+                  <input class="checkbox-form__checkbox"
+                    type="checkbox"
+                    id="task-${task.id}"
+                    ${task.completed ? 'checked="true"' : ''}>
+                  <label for="task-${task.id}"></label>
+              </form>
+              <span class="task-item__text">
+                  ${task.text}
+              </span>
+          </div>
+          <button class="task-item__delete-button default-button delete-button">
+              Удалить
+          </button>
+      </div>
+    `
+  return taskItem
+}
+
 const tasks = [
   {
       id: '1138465078061',
@@ -15,4 +41,10 @@ const tasks = [
       text: 'Выполнить ДЗ после урока',
   },
 ]
-console.log(tasks)
+
+const tasksContainer = document.querySelector('.tasks-list')
+
+tasks.forEach(task => {
+  const taskElement = createTaskElement(task)
+  tasksContainer.append(taskElement)
+})
